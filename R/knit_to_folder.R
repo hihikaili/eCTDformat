92ceb1c401993d4a193262af12d128835aa504e8
hihikaili <hihi.kaili@gmail.com>
2021-08-27 17:52:01 -0400

    correct output path

--- R/knit_to_folder.R
+++ R/knit_to_folder.R
@@ -1,7 +1,7 @@
 knit_to_folder <- function(input) {
   rmarkdown::render(
     input,
-    output_file = paste0(pathTL, Outname, Ext())
+    output_file = paste0(pathTLFs, Outname, Ext())
     envir = globalenv()
   )
 }
