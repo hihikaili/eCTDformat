40d5c99dff28e86dbcb28d5575a83c1510c6c35c
hihikaili <hihi.kaili@gmail.com>
2021-08-27 18:06:52 -0400

    modify output test

--- R/knit_to_folder.R
+++ R/knit_to_folder.R
@@ -1,8 +1,8 @@
 knit_to_folder <- function(input) {
   rmarkdown::render(
     input,
-    output_dir = pathTLFs
-    output_file = paste0(Outname, Ext())
+    output_file = paste0(Outname, Ext()),
+    output_dir = pathTLFs,
     envir = globalenv()
   )
 }
